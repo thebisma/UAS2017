31c33cb88e3fe868bf0826b23d4f490786fcc82d
iosdev <iosdev@labb509-17.exam.umn.ac.id>
2017-11-18 09:04:18 +0700

    Initial Commit

---
+++ uas bisma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uas bisma
+//
+//  Created by iosdev on 11/18/17.
+//  Copyright © 2017 ios3-umn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
