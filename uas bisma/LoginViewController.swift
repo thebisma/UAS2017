5fa17e14e72653523e4ac264451376c52588a00b
iosdev <iosdev@labb509-17.exam.umn.ac.id>
2017-11-25 10:30:32 +0700

    tttt

---
+++ uas bisma/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  uas bisma
+//
+//  Created by iosdev on 11/25/17.
+//  Copyright © 2017 ios3-umn. All rights reserved.
+//
+
+import UIKit
+import FacebookLogin
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        loginButton = LoginButton(readPermissions: [ .publicProfile, .email, .userFriends ])
+        view.addSubview(loginButton)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
